0a0313cca791bda8a7b13f45d0ac27c02a535dd1
Kit Kogan <kkogan.dev@gmail.com>
2020-10-24 22:00:48 -0500

    link form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// const Form = ({ saveCount }) => {
+//     const { value, reset, onChange } = useInputState('');
+
+// };
+
+function Form() {
+    return (
+        <div className="Form">
+          
+            <p>
+              FORM!
+            </p>
+          
+        </div>
+      );
+}
+
+
+
+export default Form;
