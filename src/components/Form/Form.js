9259bfbb8c2ca387c75a374a4daa36bba0eae39f
Kit Kogan <kkogan.dev@gmail.com>
2020-10-24 23:16:28 -0500

    add dynamic state on client

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
-const mapStateToProps = reduxState => ({
-    reduxState,
-});
+// const mapStateToProps = reduxState => ({
+//     reduxState,
+// });
 
 class Form extends Component {
     state = {
         newCount: {
             type: '',
             startCount: ''
-        }
+        },
+        countList: [],
     }
 
     handleChangeFor = (event, propertyName) => {
+        console.log('event occured');
         this.setState({
             newCount: {
                 ...this.state.newCount,
@@ -22,14 +24,28 @@ class Form extends Component {
         });
     }
 
+    addNewCount = event => {
+        event.preventDefault();
+        console.log('submit clicked')
+        // this.props.dispatch({ type: 'ADD_COUNT', payload: this.state.newCount })
+        this.setState({
+            newCount: {
+                type: '',
+                startCount: '',
+            },
+            countList: [...this.state.countList, this.state.newCount],
+        });
+        console.log('state', this.state);
+    }
+
     render() {
         return (
             <div>
                 <h3>This is the form</h3>
                 <pre>{JSON.stringify(this.state)}</pre>
                 <form onSubmit={this.addNewCount}>
-                    <input onChange={(event) => this.handleChangeFor(event, 'type')} />
-                    <input onChange={(event) => this.handleChangeFor(event, 'startCount')} />
+                    <input value={this.state.newCount.type} onChange={(event) => this.handleChangeFor(event, 'type')} />
+                    <input value={this.state.newCount.startCount} onChange={(event) => this.handleChangeFor(event, 'startCount')} />
                     <input type='submit' value='Add New Count' />
                 </form>
             </div>
